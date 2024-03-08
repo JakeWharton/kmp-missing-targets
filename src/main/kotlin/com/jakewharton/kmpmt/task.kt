d19747acf1ed3ae3421208706ca75f599b243e65
Jake Wharton <jw@squareup.com>
2024-03-07 23:50:54 -0500

    Initial commit

---
+++ src/main/kotlin/com/jakewharton/kmpmt/task.kt
@@ -0,0 +1,249 @@
+/*
+ * Copyright (C) 2024 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jakewharton.kmpmt
+
+import java.io.File
+import kotlinx.serialization.json.Json
+import org.gradle.api.DefaultTask
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.artifacts.ConfigurationContainer
+import org.gradle.api.artifacts.component.ModuleComponentIdentifier
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.api.artifacts.result.ResolvedVariantResult
+import org.gradle.api.attributes.Attribute
+import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.logging.Logging
+import org.gradle.api.provider.MapProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.provider.Provider
+import org.gradle.api.provider.SetProperty
+import org.gradle.api.tasks.CacheableTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+import org.gradle.internal.resolve.ArtifactNotFoundException
+
+@CacheableTask
+public abstract class MissingTargetsTask : DefaultTask() {
+	@get:Input
+	internal abstract val coordinateToModuleJson: MapProperty<DependencyCoordinates, String>
+
+	@get:Input
+	internal abstract val targets: SetProperty<String>
+
+	@get:Input
+	internal abstract val sourceSet: Property<String>
+
+	@get:OutputDirectory
+	internal abstract val outputDir: DirectoryProperty
+
+	@TaskAction
+	public fun execute() {
+		val logger = Logging.getLogger(MissingTargetsTask::class.java)!!
+		val jsonFormat = Json { ignoreUnknownKeys = true }
+
+		val sourceSet = sourceSet.get()
+		val outputFile = outputDir.get().file("$sourceSet.md").asFile
+		outputFile.parentFile.mkdirs()
+
+		val targets = targets.get().toSortedSet()
+
+		val coordinateToTargets = coordinateToModuleJson.get()
+			.filterKeys { coordinate ->
+				// Remove the kotlin-stdlib for two reasons:
+				// - It always has every possible target.
+				// - Its native targets are not included in its metadata.
+				coordinate.group != "org.jetbrains.kotlin" || coordinate.artifact != "kotlin-stdlib"
+			}
+			.mapValues { (coordinates, json) ->
+				try {
+					jsonFormat.decodeFromString(GradleModuleMetadata.serializer(), json)
+				} catch (e: Exception) {
+					throw IllegalStateException("Unable to parse module metadata for $coordinates")
+				}
+			}
+			.mapValues { (coordinates, metadata) ->
+				try {
+					extractEffectiveTargets(metadata).toSortedSet()
+				} catch (e: Exception) {
+					throw IllegalStateException("Unable to extract targets for $coordinates")
+				}
+			}
+			// TODO Can we get rid of this through some other filtering on attributes?
+			.filterValues { it.isNotEmpty() }
+			.toSortedMap()
+
+		// TODO This orEmpty is wrong. If you have no dependencies the result should be all targets, not none.
+		//  We probably need to retain the kotlin-stdlib and instead synthesize all possible targets here.
+		val union = coordinateToTargets.values
+			.reduceOrNull(Set<String>::intersect)
+			.orEmpty()
+		val missing = union - targets
+
+		if (logger.isDebugEnabled) {
+			logger.debug(
+				buildString {
+					append(sourceSet)
+					append(": ")
+					appendLine(targets)
+
+					if (coordinateToTargets.isNotEmpty()) {
+						appendLine()
+						for ((coordinate, coordinateTargets) in coordinateToTargets) {
+							append(coordinate)
+							append(": ")
+							appendLine(coordinateTargets)
+						}
+						appendLine()
+					}
+
+					append("union: ")
+					appendLine(union)
+					append("missing: ")
+					appendLine(missing)
+				},
+			)
+		}
+
+		outputFile.writeText(
+			buildString {
+				appendLine("# Project")
+				for (target in targets) {
+					append(" - ")
+					appendLine(target)
+				}
+
+				for ((coordinate, coordinateTargets) in coordinateToTargets) {
+					appendLine()
+					append("# `")
+					append(coordinate)
+					appendLine('`')
+					for (target in coordinateTargets) {
+						append(" - ")
+						appendLine(target)
+					}
+				}
+			},
+		)
+
+		check(missing.isEmpty()) {
+			buildString {
+				appendLine("Missing targets detected!")
+				for (target in missing) {
+					append(" - ")
+					appendLine(target)
+				}
+			}
+		}
+	}
+
+	private fun extractEffectiveTargets(metadata: GradleModuleMetadata) = buildSet {
+		for (variant in metadata.variants) {
+			if (variant.attributes.gradleDocsType != null) continue
+			if (variant.attributes.gradleUsage != "kotlin-api" && variant.attributes.gradleUsage != "java-api") continue
+			this += when (variant.attributes.kotlinPlatformType) {
+				"common", null -> continue
+				"wasm" -> {
+					// TODO check legacy wasm support behavior here
+					"wasm" + variant.attributes.kotlinWasmTarget?.replaceFirstChar(Char::uppercase).orEmpty()
+				}
+
+				"native" -> {
+					requireNotNull(variant.attributes.kotlinNativeTarget) {
+						"Variant '${variant.name}' has no Kotlin native target despite native platform type"
+					}
+						.split('_')
+						.withIndex()
+						.joinToString("") { (index, value) ->
+							if (index == 0) {
+								value
+							} else {
+								value.replaceFirstChar(Char::uppercase)
+							}
+						}
+				}
+
+				else -> variant.attributes.kotlinPlatformType
+			}
+		}
+	}
+
+	internal fun configurationToCheck(configuration: Provider<Configuration>) {
+		val dependencies = project.dependencies
+		val configurations = project.configurations
+		val moduleJsons = configuration
+			.flatMap { it.incoming.resolutionResult.rootComponent }
+			.map { root ->
+				val directDependencies = loadDependencyCoordinates(
+					logger,
+					root,
+				)
+				val moduleFiles = directDependencies.coordinates.fetchModuleFiles(
+					root.variants,
+					dependencies,
+					configurations,
+				)
+				moduleFiles.associate {
+					it.dependencyCoordinates to it.moduleFile.readText()
+				}
+			}
+
+		this.coordinateToModuleJson.set(moduleJsons)
+	}
+
+	private fun Set<DependencyCoordinates>.fetchModuleFiles(
+		variants: List<ResolvedVariantResult>,
+		dependencies: DependencyHandler,
+		configurations: ConfigurationContainer,
+	): List<DependencyCoordinatesWithModuleFile> {
+		val moduleDependencies = map {
+			dependencies.create(it.moduleCoordinate())
+		}.toTypedArray()
+
+		val withVariants = configurations.detachedConfiguration(*moduleDependencies).apply {
+			for (variant in variants) {
+				attributes {
+					val variantAttrs = variant.attributes
+					for (attrs in variantAttrs.keySet()) {
+						@Suppress("UNCHECKED_CAST")
+						it.attribute(attrs as Attribute<Any?>, variantAttrs.getAttribute(attrs)!!)
+					}
+				}
+			}
+		}.artifacts()
+
+		val withoutVariants = configurations.detachedConfiguration(*moduleDependencies).artifacts()
+
+		return (withVariants + withoutVariants).mapNotNull {
+			val coordinates = (it.id.componentIdentifier as ModuleComponentIdentifier).toDependencyCoordinates()
+			try {
+				DependencyCoordinatesWithModuleFile(coordinates, it.file)
+			} catch (e: ArtifactNotFoundException) {
+				null
+			}
+		}.distinctBy { it.dependencyCoordinates }
+	}
+
+	private data class DependencyCoordinatesWithModuleFile(
+		val dependencyCoordinates: DependencyCoordinates,
+		val moduleFile: File,
+	)
+
+	private fun ModuleComponentIdentifier.toDependencyCoordinates() = DependencyCoordinates(group, module, version)
+	private fun Configuration.artifacts() =
+		resolvedConfiguration.lenientConfiguration.allModuleDependencies.flatMap { it.allModuleArtifacts }
+}
---
+++ src/test/kotlin/com/jakewharton/kmpmt/MissingTargetsPluginFixtureTest.kt
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2024 Square, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jakewharton.kmpmt
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import assertk.assertions.isNotEmpty
+import com.google.testing.junit.testparameterinjector.TestParameter
+import com.google.testing.junit.testparameterinjector.TestParameterInjector
+import java.io.File
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(TestParameterInjector::class)
+class MissingTargetsPluginFixtureTest {
+	@Test
+	fun failure(
+		@TestParameter(
+			"first",
+		) fixtureName: String,
+	) {
+		val fixtureDir = File(fixturesDir, fixtureName)
+		createRunner(fixtureDir).buildAndFail()
+		assertExpectedFiles(fixtureDir)
+	}
+
+	@Test
+	fun allFixturesCovered() {
+		val expectedDirs = javaClass.declaredMethods
+			.filter { it.isAnnotationPresent(Test::class.java) }
+			.filter { it.parameterCount == 1 } // Assume single parameter means test parameter.
+			.flatMap { it.parameters[0].getAnnotation(TestParameter::class.java).value.toList() }
+			.sorted()
+		val actualDirs = fixturesDir.listFiles()!!
+			.filter { it.isDirectory }
+			.map { it.name }
+			.sorted()
+		assertThat(actualDirs).isEqualTo(expectedDirs)
+	}
+
+	private fun createRunner(fixtureDir: File): GradleRunner {
+		val gradleRoot = File(fixtureDir, "gradle").also { it.mkdir() }
+		File("gradle/wrapper").copyRecursively(File(gradleRoot, "wrapper"), true)
+		val androidSdkFile = File("local.properties")
+		if (androidSdkFile.exists()) {
+			androidSdkFile.copyTo(File(fixtureDir, "local.properties"), overwrite = true)
+		}
+		return GradleRunner.create()
+			.withProjectDir(fixtureDir)
+			.withDebug(true) // Run in-process
+			.withArguments(
+				"clean",
+				"assemble",
+				"kmpMissingTargets",
+				"--stacktrace",
+				"--continue",
+				"--configuration-cache",
+				versionProperty,
+			)
+			.forwardOutput()
+	}
+
+	private fun assertExpectedFiles(fixtureDir: File) {
+		val expectedDir = File(fixtureDir, "expected")
+		if (!expectedDir.exists()) {
+			throw AssertionError("Missing expected/ directory")
+		}
+
+		val expectedFiles = expectedDir.walk().filter { it.isFile }.toList()
+		assertThat(expectedFiles).isNotEmpty()
+		for (expectedFile in expectedFiles) {
+			val actualFile = File(fixtureDir, expectedFile.relativeTo(expectedDir).toString())
+			if (!actualFile.exists()) {
+				throw AssertionError("Expected $actualFile but does not exist")
+			}
+			assertThat(actualFile.readText()).isEqualTo(expectedFile.readText())
+		}
+	}
+}
+
+private val fixturesDir = File("src/test/fixtures")
+private val versionProperty = "-PkmpmtVersion=${System.getProperty("kmpmtVersion")!!}"
