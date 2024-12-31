7248c71a9747fa4b1314afe8b654f970fc03aa03
Jake Wharton <github@jakewharton.com>
2024-12-30 23:20:47 -0500

    Move constants to class of use (#53)

--- src/main/kotlin/com/jakewharton/kmpmt/MissingTargetsTask.kt
+++ src/main/kotlin/com/jakewharton/kmpmt/MissingTargetsTask.kt
@@ -369,4 +369,7 @@ public abstract class MissingTargetsTask : DefaultTask() {
 
 	private fun Configuration.artifacts() =
 		resolvedConfiguration.lenientConfiguration.allModuleDependencies.flatMap { it.allModuleArtifacts }
+
+	private val kotlinStdlib = DependencyCoordinate("org.jetbrains.kotlin", "kotlin-stdlib")
+	private val kotlinStdlibCommon = DependencyCoordinate("org.jetbrains.kotlin", "kotlin-stdlib-common")
 }
